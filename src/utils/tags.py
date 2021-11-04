aca4573b81cc787bac1c6049f3cf555497d557e6
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-11-04 19:33:32 +0600

    added pep8 code style

--- src/utils/tags.py
+++ src/utils/tags.py
@@ -1,30 +1,31 @@
 import json
 
 class Client:
+    def __init__(self):
+        pass
 
-	def __init__(self):
-		pass
+    def auth_by_email(self, d, e, pw):
+        return "{\"d\":\"" + d + "\",\"ty\":\"sin\",\"e\":\"" + \
+            e + "\",\"pw\":\"" + pw + "\"}"
 
-	def auth_by_email(self, d, e, pw):
-		return "{\"d\":\""+d+"\",\"ty\":\"sin\",\"e\":\""+e+"\",\"pw\":\""+pw+"\"}"
+    def acfl(self, t, uo):
+        return "{\"t\":\"" + t + "\", \"ty\":\"acfl\", \"uo\":\"" + uo + "\"}"
 
-	def acfl(self, t, uo):
-		return "{\"t\":\""+t+"\", \"ty\":\"acfl\", \"uo\":\""+uo+"\"}"
+    def acpc(self, t, uo, fp):
+        return "{\"t\":\"" + t + "\", \"ty\":\"acpc\", \"uo\":\"" + \
+            uo + "\", \"fp\":\"" + fp + "\"}"
 
-	def acpc(self, t, uo, fp):
-		return "{\"t\":\""+t+"\", \"ty\":\"acpc\", \"uo\":\""+uo+"\", \"fp\":\""+fp+"\"}"
+    def ac(self, fp):
+        return "{\"ty\":\"ac\", \"fp\":\"" + fp + "\"}"
 
-	def ac(self, fp):
-		return "{\"ty\":\"ac\", \"fp\":\""+fp+"\"}"
+    def pmc(self, fp, tx):
+        return json.dumps({"ty": "pmc", "m": {"fp": fp, "tx": tx}})
 
-	def pmc(self, fp, tx):
-		return json.dumps({"ty":"pmc", "m":{"fp":fp, "tx":tx}})
+    def acc(self, t, uo):
+        return "{\"t\":\"" + t + "\", \"ty\":\"acc\", \"uo\":\"" + uo + "\"}"
 
-	def acc(self, t, uo):
-		return "{\"t\":\""+t+"\", \"ty\":\"acc\", \"uo\":\""+uo+"\"}"
+    def cmc(self, tx):
+        return json.dumps({"ty": "cmc", "m": {"tx": tx}})
 
-	def cmc(self, tx):
-		return json.dumps({"ty":"cmc", "m":{"tx":tx}})
-
-	def gup(self, uo):
-		return "{\"ty\":\"gup\", \"uo\":\""+uo+"\"}"
+    def gup(self, uo):
+        return "{\"ty\":\"gup\", \"uo\":\"" + uo + "\"}"
