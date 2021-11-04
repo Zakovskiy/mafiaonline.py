d0b990a6b402da856b46773c25880bc6e7a47094
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-11-04 19:32:43 +0600

    Update md5hash.py

--- src/utils/md5hash.py
+++ src/utils/md5hash.py
@@ -1,16 +1,16 @@
 import hashlib
 
-class Client:
 
+class Client:
 	def __init__(self):
 		pass
 
-	def md5(self, string):
-		m = hashlib.md5()
-		m.update(string.encode())
-		return m.hexdigest()
+    def md5(self, string):
+        m = hashlib.md5()
+        m.update(string.encode())
+        return m.hexdigest()
 
-	def md5Salt(self, string):
-		for i in range(5):
-			string = self.md5(string + "azxsw")
-		return string
+    def md5Salt(self, string):
+        for i in range(5):
+            string = self.md5(string + "azxsw")
+        return string
